c78e74feaa81dfa8762475b0712389cbef00d583
andie5 <andieasa@googlemail.com>
2018-06-25 21:10:49 +0100

    Initial commit forked from Stephen's code

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+  // constructor(props){
+  //   super(props)
+  // }
+
+  render() {
+    return <input onChange={this.onInputChange}/>;
+  }
+
+  onInputChange(event){
+    
+  }
+}
+
+export default SearchBar;
