f24710052c599e856d483ec16895f3ee6a0f8439
andie5 <andieasa@googlemail.com>
2018-06-26 22:14:21 +0100

    chp2 WIP added thumbnails to page fore each youtube video

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar.js';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyBaout9Q8tjiYx8VBTZOKwo44Pu99iToRQ';
+const API_KEY = 'AIzaSyAAIpJckfbEP2nuGnGm8PX-LXXX7bPGFJ0';
 
 // Create a new component
 // This component should produce some HTML
-const App = () => {
-  return (
-    <div> 
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, searchTerm: 'surfboards'}, (videos) => {
+      this.setState({ videos }); /*same as this.setState({ videos: videos} when key and property are same var name)*/
+    });
+  }
+
+  render(){
+    return (
+      <div> 
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
