04bc9d0f345278a598a13c88ed980101d02bd42d
andie5 <andieasa@googlemail.com>
2018-08-09 21:36:25 +0100

    chap4-books added pages to book list,added bookdetail class

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {  selectBook } from '../actions/index';
+import {  selectBookAct } from '../actions/index';
 import { bindActionCreators } from 'redux'
-import { isAbsolute } from 'path';
+// import { isAbsolute } from 'path';
 
 export class BookList extends Component{
 
     renderList(){
-        return this.props.booksprop.map((book) => {
+        return this.props.books.map((book) => {
             return (
                 <li 
                     key={book.title}
                     onClick={ () => this.props.selectedBook(book) }
                     className="list-group-item">
                     {book.title}
-                    </li>
+                </li>
             );
         });
     }
 
     render(){
-
         return(
             <ul className="list-group col sm-4">
                 {this.renderList()}
@@ -32,17 +31,17 @@ export class BookList extends Component{
 function mapStateToProps(state) {
     // Whatever is returned will show up as props inside of BookList
     return {
-        booksprop: state.books
+        books: state.books
     };
 }
 
-// Anthing returned from this function will end up as props on the BookList container
+// Anything returned from this function will end up as props on the BookList container
 function mapDispatchToProps(dispatch){
-    // Whenever selectBook is called, the result should be passed to all of our reducers
-    return bindActionCreators({ selectedBook: selectBook}, dispatch)
+    // Whenever selectBookAct is called, the result should be passed to all of our reducers
+    return bindActionCreators({ selectedBook: selectBookAct}, dispatch)
 }
 
 
 // Promote BookList from a componenet to a container - it needs to know
-// about this new dispatch meethod, selectBook. Make it available as a prop
+// about this new dispatch meethod, selectBookAct. Make it available as a prop
 export default connect(mapStateToProps, mapDispatchToProps)(BookList)
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return[
-        {title: 'Javascript: The Good Parts'},
-        {title: 'The Philosphers stone'},
-        {title: 'Ice cream girls'},
-        {title: 'Eloquent Ruby'}
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'The Philosphers stone', pages: 39},
+        {title: 'Ice cream girls', pages:  82},
+        {title: 'Eloquent Ruby', pages: 1}
     ]
 }
