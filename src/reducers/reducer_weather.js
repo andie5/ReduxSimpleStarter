2ebb05962c277fa51d533edc436795ed7a59401c
andie5 <andieasa@googlemail.com>
2018-09-02 00:33:38 +0100

    chap5 - render weather list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+    switch(action.type){
+        case FETCH_WEATHER:
+        // concat doesn't change the existing array, it creates a new array with the contents addded so it doesn't mutate the state
+            // return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state ]; //[ city city city] NOT [city [city city]] ... notation flattens array
+    }
     return state;
 }
