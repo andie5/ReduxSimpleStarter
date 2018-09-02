698a7931e15cbeae8e8e2ee5c6d3ed9dbe07a381
andie5 <andieasa@googlemail.com>
2018-09-02 01:44:45 +0100

    chap5 Styling for charts and removed comments

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
-    console.log('Data: ', props.data);
-    console.log('Color: ', props.data);
 
     function average(data){
         return _.round(_.sum(data)/data.length);
